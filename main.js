b57ad0c76747a299e98d300b93adba49c35aac94
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-07-02 22:51:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+status = "";
+name_objects = "";
 function preload(){}
 function setup(){
     canvas = createCanvas(400,400);
     canvas.position(550,250);
+    video = createCapture(VIDEO);
+    video.hide();
 }
-function draw(){}
+function draw(){
+    image(video,0,0,400,400);
+}
+function start(){
+    objectDetector = ml5.objectDetector("cocossd",model_loaded);
+    document.getElementById("status").innerHTML = " : Dtecting Object";
+    name_objects = document.getElementById("object_name").value;
+}
+function model_loaded(){
+    console.log("Model Loaded !");
+    status = true;
+}
